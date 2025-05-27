4becb63efe3783774da9ae9b1be46ade1b580614
Martin Jerome I <martinjeromeilango@gmail.com>
2025-05-27 20:37:19 +0530

    feat
    update

--- stall_booking/settings.py
+++ stall_booking/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'django-insecure-c6i%t21r+wmjkeu+*#x)qn@mwu@5d180ow$@zc(1af(j2@cn37
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['booking.ibfeindia.com', 'booking.ibfeindia.com', '127.0.0.1']
+ALLOWED_HOSTS = ['booking.ibfeindia.com', 'booking.ibfeindia.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
